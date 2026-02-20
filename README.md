058b137db1c7cd5e458cb9ec2deebe7d2d926014
Gar <wraithgar@github.com>
2026-02-19 16:31:12 -0800

    cleanup and refactor (#1)

--- README.md
+++ README.md
@@ -1,23 +1,10 @@
-# node-promise-retry
+# @gar/promise-retry
 
-[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency status][david-dm-image]][david-dm-url] [![Dev Dependency status][david-dm-dev-image]][david-dm-dev-url] [![Greenkeeper badge][greenkeeper-image]][greenkeeper-url]
-
-[npm-url]:https://npmjs.org/package/promise-retry
-[downloads-image]:http://img.shields.io/npm/dm/promise-retry.svg
-[npm-image]:http://img.shields.io/npm/v/promise-retry.svg
-[travis-url]:https://travis-ci.org/IndigoUnited/node-promise-retry
-[travis-image]:http://img.shields.io/travis/IndigoUnited/node-promise-retry/master.svg
-[david-dm-url]:https://david-dm.org/IndigoUnited/node-promise-retry
-[david-dm-image]:https://img.shields.io/david/IndigoUnited/node-promise-retry.svg
-[david-dm-dev-url]:https://david-dm.org/IndigoUnited/node-promise-retry?type=dev
-[david-dm-dev-image]:https://img.shields.io/david/dev/IndigoUnited/node-promise-retry.svg
-[greenkeeper-image]:https://badges.greenkeeper.io/IndigoUnited/node-promise-retry.svg
-[greenkeeper-url]:https://greenkeeper.io/
+This is a fork of [promise-retry](https://npm.im/promise-retry) to keep its dependencies current.
 
 Retries a function that returns a promise, leveraging the power of the [retry](https://github.com/tim-kos/node-retry) module to the promises world.
 
-There's already some modules that are able to retry functions that return promises but
-they were rather difficult to use or do not offer an easy way to do conditional retries.
+There's already some modules that are able to retry functions that return promises but they were rather difficult to use or do not offer an easy way to do conditional retries.
 
 
 ## Installation
@@ -29,8 +16,7 @@ they were rather difficult to use or do not offer an easy way to do conditional 
 
 ### promiseRetry(fn, [options])
 
-Calls `fn` until the returned promise ends up fulfilled or rejected with an error different than
-a `retry` error.   
+Calls `fn` until the returned promise ends up fulfilled or rejected with an error different than a `retry` error.
 The `options` argument is an object which maps to the [retry](https://github.com/tim-kos/node-retry) module options:
 
 - `retries`: The maximum amount of times to retry the operation. Default is `10`.
@@ -40,11 +26,9 @@ The `options` argument is an object which maps to the [retry](https://github.com
 - `randomize`: Randomizes the timeouts by multiplying with a factor between `1` to `2`. Default is `false`.
 
 
-The `fn` function will receive a `retry` function as its first argument that should be called with an error whenever you want to retry `fn`. The `retry` function will always throw an error.   
+The `fn` function will receive a `retry` function as its first argument that should be called with an error whenever you want to retry `fn`. The `retry` function will always throw an error.
 If there are retries left, it will throw a special `retry` error that will be handled internally to call `fn` again.
 If there are no retries left, it will throw the actual error passed to it.
-
-If you prefer, you can pass the options first using the alternative function signature `promiseRetry([options], fn)`.
 
 ## Example
 ```js
@@ -83,11 +67,9 @@ promiseRetry(function (retry, number) {
 });
 ```
 
-
 ## Tests
 
 `$ npm test`
-
 
 ## License
 
---
+++ test/index.js
@@ -0,0 +1,164 @@
+const t = require('node:test')
+const a = require('node:assert')
+const { setTimeout } = require('node:timers/promises')
+
+const promiseRetry = require('../')
+
+t.suite('promise-retry', () => {
+  t.test('should call fn again if retry was called', async t => {
+    let count = 0
+    const value = await promiseRetry(async retry => {
+      count += 1
+      await setTimeout(10)
+      if (count <= 2) {
+        retry(new Error('foo'))
+      }
+      return 'final'
+    }, { factor: 1, minTimeout: 100 })
+    a.equal(value, 'final')
+    a.equal(count, 3)
+  })
+
+  t.test('should call fn with the attempt number', async t => {
+    let count = 0
+    const value = await promiseRetry(async (retry, number) => {
+      count += 1
+      a.equal(count, number)
+      await setTimeout(10)
+      if (count <= 2) {
+        retry(new Error('foo'))
+      }
+      return 'final'
+    }, { factor: 1, minTimeout: 100 })
+    a.equal(value, 'final')
+    a.equal(count, 3)
+  })
+
+  t.test('should not retry on fulfillment if retry was not called', async t => {
+    let count = 0
+    const value = await promiseRetry(async () => {
+      count += 1
+      await setTimeout(10)
+      return 'final'
+    })
+    a.equal(value, 'final')
+    a.equal(count, 1)
+  })
+
+  t.test('should not retry on rejection if retry was not called', async t => {
+    let count = 0
+    await a.rejects(
+      promiseRetry(async () => {
+        count += 1
+        await setTimeout(10)
+        throw new Error('foo')
+      }),
+      { message: 'foo' }
+    )
+    a.equal(count, 1)
+  })
+
+  t.test('should not retry on error if number of retries is 0', async t => {
+    let count = 0
+
+    await a.rejects(
+      promiseRetry(async retry => {
+        count += 1
+        await setTimeout(10)
+        retry(new Error('foo'))
+      }, { retries: 0 }),
+      { message: 'foo' }
+    )
+    a.equal(count, 1)
+  })
+
+  t.test('should reject the promise if the retries were exceeded', async t => {
+    let count = 0
+
+    await a.rejects(
+      promiseRetry(async retry => {
+        count += 1
+        await setTimeout(10)
+        retry(new Error('foo'))
+      }, { retries: 2, factor: 1, minTimeout: 100 }),
+      { message: 'foo' }
+    )
+    a.equal(count, 3)
+  })
+
+  t.test('should pass options to the underlying retry module', async t => {
+    let count = 0
+
+    await a.rejects(
+      promiseRetry(async retry => {
+        await setTimeout(10)
+        if (count < 2) {
+          count += 1
+          retry(new Error('foo'))
+        }
+        return 'final'
+      }, { retries: 1, factor: 1, minTimeout: 100 }),
+      { message: 'foo' }
+    )
+  })
+
+  t.test('should convert synchronous functions into promises', async t => {
+    const value = await promiseRetry(() => { return 'final' }, { factor: 1 })
+    a.equal(value, 'final')
+  })
+
+  t.test('should convert synchronous throws into promises', async t => {
+    await a.rejects(
+      promiseRetry(() => { throw new Error('foo') }, { retries: 1, factor: 1 }),
+      { message: 'foo' }
+    )
+  })
+
+  t.test('should not crash on undefined rejections', async t => {
+    await a.rejects(
+      promiseRetry(() => {
+        // eslint-disable-next-line no-throw-literal
+        throw undefined
+      }, { retries: 1, factor: 1 }),
+      undefined
+    )
+    await a.rejects(
+      promiseRetry((retry) => {
+        retry()
+      }, { retries: 1, factor: 1, minTimeout: 100 }),
+      undefined
+    )
+  })
+
+  t.test('should retry if retry() was called with undefined', async t => {
+    let count = 0
+
+    const value = await promiseRetry(
+      async retry => {
+        count += 1
+        await setTimeout(10)
+
+        if (count <= 2) {
+          retry()
+        }
+        return 'final'
+      }, { factor: 1, minTimeout: 100 })
+    a.equal(value, 'final')
+    a.equal(count, 3)
+  })
+
+  t.test('should work with several retries in the same chain', async t => {
+    let count = 0
+
+    await a.rejects(
+      promiseRetry(async retry => {
+        count += 1
+
+        await setTimeout(10)
+        retry(new Error('foo'))
+      }, { retries: 1, factor: 1, minTimeout: 100 }),
+      { message: 'foo' }
+    )
+    a.equal(count, 2)
+  })
+})
