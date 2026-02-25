e9612d80bf746e62bb8ea9458a09003334b00f9c
Michael Garvin <gar+gh@danger.computer>
2026-02-25 14:28:54 -0800

    chore: test from main export

--- test/retry.js
+++ test/retry.js
@@ -1,334 +1,238 @@
 const t = require('node:test')
 const a = require('node:assert')
-
-const retry = require('../lib/retry')
+const { setTimeout } = require('node:timers/promises')
+const { promiseRetry } = require('..')
 
 t.suite('retry', () => {
   t.suite('timeouts', () => {
-    t.test('defaults', t => {
-      const { timeouts } = retry.operation()
-      a.deepEqual(timeouts, [1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000])
+    t.test('defaults', async t => {
+      await promiseRetry((retry, number, { timeouts }) => {
+        a.deepEqual(timeouts, [1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000])
+      })
     })
 
-    t.test('with minTimeout and randomize', t => {
+    t.test('with minTimeout and randomize', async t => {
       const minTimeout = 5000
-      const { timeouts } = retry.operation({
+      await promiseRetry((retry, number, { timeouts }) => {
+        a.equal(timeouts.length, 10)
+        a.ok(timeouts[0] > minTimeout)
+        const sorted = timeouts.sort((a, b) => a - b)
+        a.deepEqual(timeouts, sorted)
+      }, {
         minTimeout,
         randomize: true
       })
-
-      a.equal(timeouts.length, 10)
-      a.ok(timeouts[0] > minTimeout)
-      const sorted = timeouts.sort((a, b) => a - b)
-      a.deepEqual(timeouts, sorted)
     })
 
-    t.test('passed timeouts are copied', t => {
+    t.test('passed timeouts are copied', async t => {
       const timeoutsArray = [1000, 2000, 3000]
-      const { timeouts } = retry.operation(timeoutsArray)
-      a.deepEqual(timeouts, timeoutsArray)
-      a.notStrictEqual(timeouts, timeoutsArray)
+      await promiseRetry((retry, number, { timeouts }) => {
+        a.deepEqual(timeouts, timeoutsArray)
+        a.notStrictEqual(timeouts, timeoutsArray)
+      }, timeoutsArray)
     })
 
-    t.test('within boundaries', t => {
+    t.test('within boundaries', async t => {
       const minTimeout = 1000
       const maxTimeout = 10000
-      const { timeouts } = retry.operation({ minTimeout, maxTimeout })
-      for (const timeout of timeouts) {
-        a.ok(timeout >= minTimeout)
-        a.ok(timeout <= maxTimeout)
-      }
+      await promiseRetry((retry, number, { timeouts }) => {
+        for (const timeout of timeouts) {
+          a.ok(timeout >= minTimeout)
+          a.ok(timeout <= maxTimeout)
+        }
+      }, { minTimeout, maxTimeout })
     })
 
-    t.test('incremental', t => {
-      // TODO defaults are already tested in whole
-      const { timeouts } = retry.operation()
-      const sorted = timeouts.sort((a, b) => a - b)
-      a.deepEqual(timeouts, sorted)
+    t.test('incremental', async t => {
+      await promiseRetry((retry, number, { timeouts }) => {
+        const sorted = timeouts.sort((a, b) => a - b)
+        a.deepEqual(timeouts, sorted)
+      }, { retries: 5, factor: 0.5 })
     })
 
-    t.test('incremental for factors less than one', t => {
-      const { timeouts } = retry.operation({
+    t.test('incremental for factors less than one', async t => {
+      await promiseRetry((retry, number, { timeouts }) => {
+        const expected = [250, 500, 1000]
+        a.deepEqual(expected, timeouts)
+      }, {
         retries: 3,
         factor: 0.5
       })
-
-      const expected = [250, 500, 1000]
-      a.deepEqual(expected, timeouts)
     })
 
-    t.test('retries', t => {
-      const { timeouts } = retry.operation({ retries: 2 })
-      a.equal(timeouts.length, 2)
+    t.test('retries', async t => {
+      await promiseRetry((retry, number, { timeouts }) => {
+        a.equal(timeouts.length, 2)
+      }, { retries: 2 })
     })
 
-    t.test('minTimeout > maxTimeout', t => {
-      a.throws(() => {
-        retry.operation({ minTimeout: 100, maxTimeout: 1 })
-      }, { message: 'minTimeout is greater than maxTimeout' }
+    t.test('minTimeout > maxTimeout', async t => {
+      a.rejects(
+        () => promiseRetry((retry, number, { timeouts }) => {}, { minTimeout: 100, maxTimeout: 1 }),
+        { message: 'minTimeout is greater than maxTimeout' }
       )
     })
   })
 
   t.suite('operation', () => {
-    t.test('reset', (t, done) => {
-      const error = new Error('some error')
-      const operation = retry.operation([1, 2, 3])
-      let attempts = 0
-
-      let expectedFinishes = 1
-      let finishes = 0
-
-      const fn = function () {
-        operation.attempt(function (currentAttempt) {
-          attempts++
-          a.equal(currentAttempt, attempts)
-          if (operation.retry(error)) {
-            return
-          }
-
-          finishes++
-          a.equal(expectedFinishes, finishes)
-          a.strictEqual(attempts, 4)
-          a.strictEqual(operation.attempts, attempts)
-          a.strictEqual(operation.mainError, error)
-
-          if (finishes < 2) {
-            attempts = 0
-            expectedFinishes++
-            operation.reset()
-            fn()
-          } else {
-            done()
-          }
-        })
-      }
-      fn()
+    t.test('reset', async t => {
+      let reset = false
+      let count = 0
+      const error = new Error('retrying')
+      await promiseRetry(async (retry, number, operation) => {
+        count += 1
+        await setTimeout(10)
+        if (count <= 2) {
+          retry(new Error('foo'))
+        }
+        if (!reset) {
+          operation.reset()
+          reset = true
+          return retry(error)
+        }
+        return 'final'
+      }, { factor: 1, minTimeout: 100 })
+      a.equal(count, 4)
     })
 
     t.suite('errors', () => {
-      t.test('main error returns most frequent error', (t, done) => {
-        const operation = retry.operation([1, 2, 3])
-        let attempts = 0
-        operation.attempt(function (currentAttempt) {
-          attempts++
-          if (!operation.retry(new Error(`${attempts ? 'other errors' : 'first error'}`))) {
-            a.strictEqual(attempts, 4)
-            a.strictEqual(operation.attempts, attempts)
-            a.deepStrictEqual(operation.mainError, new Error('other errors'))
-            done()
-          }
-        })
+      t.test('main error aggregates most frequent error', async t => {
+        let mainError
+        await a.rejects(() => promiseRetry(async (retry, number, operation) => {
+          mainError = operation.mainError
+          retry(new Error(`${number !== 1 ? 'other errors' : 'first error'}`))
+        }, [1, 2, 3]))
+        a.equal(mainError.message, 'other errors')
       })
 
-      t.test('main error returns last error', (t, done) => {
-        const operation = retry.operation([1, 2, 3])
-        let attempts = 0
-        operation.attempt(function (currentAttempt) {
-          attempts++
-          if (!operation.retry(new Error(`Error ${attempts}`))) {
-            a.strictEqual(attempts, 4)
-            a.strictEqual(operation.attempts, attempts)
-            a.deepStrictEqual(operation.mainError, new Error('Error 4'))
-            a.deepStrictEqual(operation.errors, [
-              new Error('Error 1'),
-              new Error('Error 2'),
-              new Error('Error 3'),
-              new Error('Error 4')
-            ])
-            done()
+      t.test('main error returns last error if none are most frequent', async t => {
+        await a.rejects(() => promiseRetry(async (retry, number, operation) => {
+          if (number > 1) {
+            a.equal(operation.mainError.message, `error ${number}`)
           }
-        })
+          retry(new Error(`Error ${number}`))
+        }, [1, 2, 3, 4]))
       })
 
-      t.test('main error null if no errors', t => {
-        const operation = retry.operation()
-        operation.attempt(function () {
-          return true
-        })
-        a.equal(operation.mainError, null)
+      t.test('main error null if no errors', async t => {
+        const result = await promiseRetry(async (retry, number, operation) => operation.mainError)
+        a.equal(result, null)
       })
     })
 
-    t.test('retry', (t, done) => {
-      const error = new Error('some error')
-      const operation = retry.operation([1, 2, 3])
-      let attempts = 0
-
-      operation.attempt(function (currentAttempt) {
-        attempts++
-        a.equal(currentAttempt, attempts)
-        if (!operation.retry(error)) {
-          a.strictEqual(attempts, 4)
-          a.strictEqual(operation.attempts, attempts)
-          a.strictEqual(operation.mainError, error)
-          done()
-        }
-      })
-    })
-
-    t.test('retry forever does not grow errors infinitely', (t, done) => {
+    t.test('retry forever does not grow errors infinitely', async t => {
       const retries = 3
-      const operation = retry.operation({ retries, forever: true, minTimeout: 1, maxTimeout: 10 })
       let attempts = 0
-
-      operation.attempt(function (currentAttempt) {
+      await promiseRetry(async (retry, number, operation) => {
         attempts++
-        a.equal(currentAttempt, attempts)
-        if (attempts !== 12 && operation.retry(new Error(`error ${attempts}`))) {
-          return
+        const error = new Error(`error ${number}`)
+        if (number !== 12 && retry(error)) {
+          throw error
         }
-
-        a.strictEqual(attempts, 12)
-        a.strictEqual(operation.attempts, attempts)
+        a.strictEqual(operation.attempts, number)
         a.strictEqual(operation.mainError.message, `error ${retries}`)
         a.equal(operation.errors.length, retries)
-        done()
-      })
+      }, { retries, forever: true, minTimeout: 1, maxTimeout: 10 })
+      a.equal(attempts, 12)
     })
 
-    t.test('infinite retries', (t, done) => {
+    t.test('retries: Infinity', async t => {
       const error = new Error('some error')
-      const operation = retry.operation({ retries: Infinity, minTimeout: 1, maxTimeout: 10 })
       let attempts = 0
-
-      operation.attempt(function (currentAttempt) {
+      const result = await promiseRetry(async (retry, number, operation) => {
         attempts++
-        a.equal(currentAttempt, attempts)
-        if (attempts !== 6 && operation.retry(error)) {
-          return
+        a.equal(number, attempts)
+        if (attempts !== 12 && retry(error)) {
+          throw error
         }
-
-        a.strictEqual(attempts, 6)
         a.strictEqual(operation.attempts, attempts)
         a.strictEqual(operation.mainError, error)
-        done()
-      })
+        return `return ${attempts}`
+      }, { retries: Infinity, minTimeout: 1, maxTimeout: 10 })
+      a.equal(attempts, 12)
+      a.equal(result, 'return 12')
     })
 
-    t.test('retry forever no retries', (t, done) => {
+    t.test('forever: true, retries: null', async t => {
       const error = new Error('some error')
       const delay = 50
-      const operation = retry.operation({
-        retries: null,
-        forever: true,
-        minTimeout: delay,
-        maxTimeout: delay
-      })
-
       let attempts = 0
       const startTime = new Date().getTime()
 
-      operation.attempt(function (currentAttempt) {
+      await promiseRetry(async (retry, number, operation) => {
         attempts++
-        a.equal(currentAttempt, attempts)
-        if (attempts !== 4 && operation.retry(error)) {
-          return
+        a.equal(number, attempts)
+        if (attempts !== 4 && retry(error)) {
+          throw error
         }
-
         const endTime = new Date().getTime()
         const minTime = startTime + (delay * 3)
         const maxTime = minTime + 20 // add a little headroom for code execution time
         a.ok(endTime >= minTime)
         a.ok(endTime < maxTime)
-        a.strictEqual(attempts, 4)
         a.strictEqual(operation.attempts, attempts)
         a.strictEqual(operation.mainError, error)
-        done()
+        return 'last'
+      }, {
+        retries: null,
+        forever: true,
+        minTimeout: delay,
+        maxTimeout: delay
       })
+      a.equal(attempts, 4)
     })
 
-    t.test('stop', (t, done) => {
+    t.test('stop', async t => {
       const error = new Error('some error')
-      const operation = retry.operation([1, 2, 3])
       let attempts = 0
+      await a.rejects(() =>
+        promiseRetry(async (retry, number, operation) => {
+          attempts++
+          a.equal(number, attempts)
 
-      operation.attempt(function (currentAttempt) {
-        attempts++
-        a.equal(currentAttempt, attempts)
+          if (attempts === 2) {
+            operation.stop()
 
-        if (attempts === 2) {
-          operation.stop()
-
-          a.strictEqual(attempts, 2)
-          a.strictEqual(operation.attempts, attempts)
-          a.strictEqual(operation.mainError, error)
-          done()
-        }
-
-        operation.retry(error)
-      })
+            a.strictEqual(operation.attempts, attempts)
+            a.strictEqual(operation.mainError, error)
+          }
+          retry(error)
+        }, [1, 2, 3]))
+      a.equal(attempts, 2)
     })
 
-    t.test('maxRetryTime', (t, done) => {
+    t.test('maxRetryTime', async t => {
       const error = new Error('some error')
       const maxRetryTime = 30
-      const operation = retry.operation({
-        minTimeout: 1,
-        maxRetryTime
-      })
-      let attempts = 0
-
-      const longAsyncFunction = function (wait, callback) {
-        setTimeout(callback, wait)
-      }
-
       const startTime = new Date().getTime()
-      operation.attempt(function (currentAttempt) {
+      let attempts = 0
+      await a.rejects(() => promiseRetry(async (retry, number, operation) => {
         attempts++
-        a.equal(currentAttempt, attempts)
+        a.equal(number, attempts)
 
         if (attempts !== 2) {
-          operation.retry(error)
-        } else {
-          const curTime = new Date().getTime()
-          longAsyncFunction(maxRetryTime - (curTime - startTime - 1), function () {
-            if (operation.retry(error)) {
-              a.fail('timeout should be occurred')
-              return
-            }
-
-            a.strictEqual(operation.mainError, error)
-            done()
-          })
+          return retry(error)
         }
-      })
-    })
-
-    t.test('errors preserved when maxRetryTime exceeded', (t, done) => {
-      const error = new Error('some error')
-      const maxRetryTime = 30
-      const operation = retry.operation({
+        const curTime = new Date().getTime()
+        await setTimeout(maxRetryTime - (curTime - startTime - 1))
+        a.strictEqual(operation.mainError, error)
+        if (retry(new Error('error past timeout'))) {
+          a.fail('timeout should have occurred')
+        }
+      }, {
         minTimeout: 1,
         maxRetryTime
-      })
-
-      const longAsyncFunction = function (wait, callback) {
-        setTimeout(callback, wait)
-      }
-
-      const startTime = new Date().getTime()
-      operation.attempt(function () {
-        const curTime = new Date().getTime()
-        longAsyncFunction(maxRetryTime - (curTime - startTime - 1), function () {
-          if (operation.retry(error)) {
-            a.fail('timeout should occur')
-            return
-          }
-
-          a.strictEqual(operation.mainError, error)
-          done()
-        })
-      })
+      }), { message: 'error past timeout' })
+      a.equal(attempts, 2)
     })
 
-    t.test('unref', t => {
+    t.test('unref', async t => {
       const error = new Error('some error')
-      const operation = retry.operation({ unref: true })
-      operation.attempt(function () {
-        operation.retry(error)
-      })
+      promiseRetry(async retry => {
+        retry(error)
+      }, { unref: true })
       a.ok('event loop not blocked')
+      // If the setTimeout doesn't get unreffed tests will fail with 'Promise resolution is still pending but the event loop has already resolved'
     })
   })
 })
